2cdc54bddacc8546a3eea5e0b4f4f2e0b1db3433
Domenico Di Tommaso <d.ditommaso@zcscompany.com>
2026-05-22 10:14:44 +0200

    feat: implement coins economy update

--- src/main/java/it/polimi/tiw/civitas/dao/NationResourceDAO.java
+++ src/main/java/it/polimi/tiw/civitas/dao/NationResourceDAO.java
@@ -14,7 +14,7 @@ public class NationResourceDAO {
     public void initializeResources(Connection connection, int nationId) throws SQLException {
         String sql = """
                 INSERT INTO nation_resources (nation_id, coins, culture_points, energy_points)
-                VALUES (?, 0, 0, 0)
+                VALUES (?, 100, 0, 0)
                 """;
 
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
